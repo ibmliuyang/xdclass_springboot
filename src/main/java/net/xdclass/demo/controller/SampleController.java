085092994a4ce5a33cf82f232fe4b521b5e6fd1a
javaxiaoliu <liuyang2006@126.com>
2018-12-21 18:38:38 +0800

    mac13  get rest

--- src/main/java/net/xdclass/demo/controller/SampleController.java
+++ src/main/java/net/xdclass/demo/controller/SampleController.java
@@ -3,17 +3,15 @@ package net.xdclass.demo.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.naming.Name;
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
-@EnableAutoConfiguration
-//@RestController
+//@Controller
+//@EnableAutoConfiguration
+@RestController
 public class SampleController {
 
 
@@ -24,6 +22,7 @@ public class SampleController {
     }
 
     @RequestMapping("/test1")
+    @ResponseBody
     Map test1(){
        Map map = new HashMap();
        map.put("name", "zhangsan");
@@ -31,4 +30,20 @@ public class SampleController {
        return map;
     }
 
+    private Map<String,String> params = new HashMap<>();
+   /* @RequestMapping(path = "/{city_id}/{user_id}",method = RequestMethod.GET)
+    public Object findUser(@PathVariable("city_id") String cityId,@PathVariable("user_id") String userId){
+        params.clear();
+        params.put("cityId", cityId);
+        params.put("userId", userId);
+        return params;
+    }*/
+
+    @GetMapping(path = "/{city_id}/{user_id}")
+    public Object findUserGetMapping(@PathVariable("city_id") String cityId,@PathVariable("user_id") String userId){
+        params.clear();
+        params.put("cityId", cityId);
+        params.put("userId", userId);
+        return params;
+    }
 }
